71dffceebc30c2ca6c9c0ec5451f111e35fca9a4
himanshu <himanshu@moveta.com>
2017-11-01 13:07:50 -0400

    count and details for inactive listings based on find_by_address added to listings

--- common/models/listings.js
+++ common/models/listings.js
@@ -29,11 +29,47 @@ module.exports = function(Listings) {
     Listings.find({
       where: filterData[0],
     }, function(err, listings) {
-      cb(null, JSON.parse(JSON.stringify((listings)), {
-      }));
+      cb(null, JSON.parse(JSON.stringify((listings)), {}));
     });
   },
   Listings.remoteMethod('findFilteredListing', {
+    accepts: {arg: 'filterData', type: 'array'},
+    returns: {arg: 'items', type: Listings},
+  });
+
+  Listings.findFilteredListingInactiveCount = function(filterData, cb) {
+    filterData[0]['addr'] = filterData[0].address;
+    if (filterData[0].unit) {
+      filterData[0]['aptNum'] = filterData[0].unit;
+    }
+    filterData[0]['isActive'] = 0;
+    filterData[0]['listingsTypeId'] = {'neq': 2};
+    delete filterData[0]['address'];
+    Listings.count(filterData[0], function(err, count) {
+      cb(null, count);
+    });
+  },
+  Listings.remoteMethod('findFilteredListingInactiveCount', {
+    accepts: {arg: 'filterData', type: 'array'},
+    returns: {arg: 'totalItems', type: Listings},
+  });
+
+  Listings.findFilteredListingInactive = function(filterData, cb) {
+    filterData[0]['addr'] = filterData[0].address;
+    if (filterData[0].unit) {
+      filterData[0]['aptNum'] = filterData[0].unit;
+    }
+    filterData[0]['isActive'] = 0;
+    filterData[0]['listingsTypeId'] = {'neq': 2};
+    delete filterData[0]['address'];
+    Listings.findOne({
+      where: filterData[0],
+      order: 'timestampSql DESC',
+    }, function(err, listings) {
+      cb(null, JSON.parse(JSON.stringify((listings)), {}));
+    });
+  },
+  Listings.remoteMethod('findFilteredListingInactive', {
     accepts: {arg: 'filterData', type: 'array'},
     returns: {arg: 'items', type: Listings},
   });
