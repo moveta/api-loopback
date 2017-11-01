2dd20abba470195e777ad54b4c84e3c51b5327ad
himanshu <himanshu@moveta.com>
2017-11-01 16:26:08 -0400

    count and details for unlisted_listings based on find_by_address added

--- common/models/unlisted-listings.js
+++ common/models/unlisted-listings.js
@@ -1,5 +1,35 @@
 'use strict';
 
 module.exports = function(UnlistedListings) {
+  UnlistedListings.findUnlistedListingsByAddressCount = function(filterData, cb) {
+    filterData[0]['addr'] = filterData[0].address;
+    delete filterData[0]['address'];
+    delete filterData[0]['unit'];
+    UnlistedListings.count(filterData[0], function(err, count) {
+      cb(null, count);
+    });
+  },
+  UnlistedListings.remoteMethod('findUnlistedListingsByAddressCount', {
+    accepts: {arg: 'filterData', type: 'array'},
+    returns: {arg: 'totalItems', type: UnlistedListings},
+  });
 
+  UnlistedListings.findUnlistedListingsByAddress = function(filterData, cb) {
+    filterData[0]['addr'] = filterData[0].address;
+    delete filterData[0]['address'];
+    delete filterData[0]['unit'];
+    UnlistedListings.findOne({
+      where: filterData[0],
+      include: [
+        'neighbourhoods',
+        'cities',
+      ],
+    }, function(err, unlistedListings) {
+      cb(null, JSON.parse(JSON.stringify((unlistedListings)), {}));
+    });
+  },
+  UnlistedListings.remoteMethod('findUnlistedListingsByAddress', {
+    accepts: {arg: 'filterData', type: 'array'},
+    returns: {arg: 'items', type: UnlistedListings},
+  });
 };
