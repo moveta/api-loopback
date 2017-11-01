2f322cf212f4ebd894bf3741a8dcee4f6577d910
himanshu <himanshu@moveta.com>
2017-11-01 11:37:40 -0400

    listings updated

--- common/models/listings.js
+++ common/models/listings.js
@@ -2,17 +2,14 @@
 
 module.exports = function(Listings) {
   Listings.findFilteredListingCount = function(filterData, cb) {
-    var address = (filterData[0].address) ? filterData[0].address : '';
-    var unit = (filterData[0].unit) ? filterData[0].unit : '';
-    var neighbourhood = (filterData[0].neighbourhoodSlug) ? filterData[0].neighbourhoodSlug : '';
-    var city = (filterData[0].cityMunicipalitySlug) ? filterData[0].cityMunicipalitySlug : '';
-    Listings.count({
-      addr: address,
-      neighbourhoodSlug: neighbourhood,
-      cityMunicipalitySlug: city,
-      isActive: 1,
-      listingsTypeId: {neq: 2},
-    }, function(err, count) {
+    filterData[0]['addr'] = filterData[0].address;
+    if (filterData[0].unit) {
+      filterData[0]['aptNum'] = filterData[0].unit;
+    }
+    filterData[0]['isActive'] = 1;
+    filterData[0]['listingsTypeId'] = {'neq': 2};
+    delete filterData[0]['address'];
+    Listings.count(filterData[0], function(err, count) {
       cb(null, count);
     });
   },
@@ -22,18 +19,15 @@ module.exports = function(Listings) {
   });
 
   Listings.findFilteredListing = function(filterData, cb) {
-    var address = (filterData[0].address) ? filterData[0].address : '';
-    var unit = (filterData[0].unit) ? filterData[0].unit : '';
-    var neighbourhood = (filterData[0].neighbourhoodSlug) ? filterData[0].neighbourhoodSlug : '';
-    var city = (filterData[0].cityMunicipalitySlug) ? filterData[0].cityMunicipalitySlug : '';
+    filterData[0]['addr'] = filterData[0].address;
+    if (filterData[0].unit) {
+      filterData[0]['aptNum'] = filterData[0].unit;
+    }
+    filterData[0]['isActive'] = 1;
+    filterData[0]['listingsTypeId'] = {'neq': 2};
+    //delete filterData[0]['address'];
     Listings.find({
-      where: {
-        addr: address,
-        neighbourhoodSlug: neighbourhood,
-        cityMunicipalitySlug: city,
-        isActive: 1,
-        listingsTypeId: {neq: 2},
-      },
+      where: filterData[0],
     }, function(err, listings) {
       cb(null, JSON.parse(JSON.stringify((listings)), {
       }));
