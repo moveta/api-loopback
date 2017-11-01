b5e3964cb3ffebfd2da3493f05a0482484539481
himanshu <himanshu@moveta.com>
2017-11-01 10:08:13 -0400

    listings count and find_by_address is now functional

--- common/models/listings.js
+++ common/models/listings.js
@@ -1,5 +1,48 @@
 'use strict';
 
 module.exports = function(Listings) {
+  Listings.findFilteredListingCount = function(filterData, cb) {
 
+    var address = (filterData[0].address) ? filterData[0].address : '';
+    var unit = (filterData[0].unit) ? filterData[0].unit : '';
+    var neighbourhood = (filterData[0].neighbourhoodSlug) ? filterData[0].neighbourhoodSlug : '';
+    var city = (filterData[0].cityMunicipalitySlug) ? filterData[0].cityMunicipalitySlug : '';
+    Listings.count({
+      addr: address,
+      neighbourhoodSlug: neighbourhood,
+      cityMunicipalitySlug: city,
+      isActive: 1,
+      listingsTypeId: {neq: 2},
+    }, function(err, count) {
+      cb(null, count);
+    });
+  },
+  Listings.remoteMethod('findFilteredListingCount', {
+    accepts: {arg: 'filterData', type: 'array'},
+    returns: {arg: 'totalItems', type: Listings},
+  });
+
+  Listings.findFilteredListing = function(filterData, cb) {
+
+    var address = (filterData[0].address) ? filterData[0].address : '';
+    var unit = (filterData[0].unit) ? filterData[0].unit : '';
+    var neighbourhood = (filterData[0].neighbourhoodSlug) ? filterData[0].neighbourhoodSlug : '';
+    var city = (filterData[0].cityMunicipalitySlug) ? filterData[0].cityMunicipalitySlug : '';
+    Listings.find({
+      where: {
+        addr: address,
+        neighbourhoodSlug: neighbourhood,
+        cityMunicipalitySlug: city,
+        isActive: 1,
+        listingsTypeId: {neq: 2},
+      },
+    }, function(err, listings) {
+      cb(null, Object.assign({}, JSON.parse(JSON.stringify((listings))), {
+      }));
+    });
+  },
+  Listings.remoteMethod('findFilteredListing', {
+    accepts: {arg: 'filterData', type: 'array'},
+    returns: {arg: 'items', type: Listings},
+  });
 };
